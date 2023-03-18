61466eb7652661398624f91eb7aa0109544a81d9
SchmidtLaci (Warden) <schmidtlacus@gmail.com>
2023-03-18 20:16:34 +0100

    shit

--- Project_Bank/Tool/tool.ino
+++ Project_Bank/Tool/tool.ino
@@ -1,18 +1,6 @@
+#include "CardService.h"
 
-
-#include <SPI.h>
-#include <MFRC522.h>
-
-
-
-void printkey(MFRC522::MIFARE_Key &key)
-{
-	for (byte i = 0; i < 6; i++)
-	{
-		Serial.print(key.keyByte[i], HEX);
-		Serial.print(" ");
-	}
-}
+CardService cardservice;
 
 class Menu;
 class Listdetect;
@@ -30,6 +18,7 @@ public:
 State *MENU = nullptr;
 State *LISTDETECT = nullptr;
 State *CARDNEAR = nullptr;
+State *READ_WRITE = nullptr;
 State *state = nullptr; // MENU;
 
 String WFS();
@@ -43,7 +32,8 @@ public:
 		Serial.println("RFC debug tool:");
 		Serial.println("commands:");
 		Serial.println("0 is card, present loop");
-		Serial.println("1 is card keep");
+		Serial.println("1 Card continuity check");
+		Serial.println("2 manage card content");
 		String cm;
 		while ((cm = WFS()) != "q")
 		{
@@ -57,6 +47,11 @@ public:
 			{
 				return CARDNEAR;
 			}
+			if (cm == "2")
+			{
+				return READ_WRITE;
+			}
+			
 		}
 		return this;
 	}
@@ -94,7 +89,7 @@ public:
 					byte atqa_size = 2;
 					status = mfrc522.PICC_RequestA(atqa_answer, &atqa_size);
 					Serial.println(mfrc522.GetStatusCodeName(status));
-				}					
+				}
 				if (cm == "d")
 				{
 					mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
@@ -114,54 +109,25 @@ public:
 				if (cm == "c")
 				{
 					byte blockAddr = 11;
-  					byte trailerBlock = 11;
+					byte trailerBlock = 11;
 					while ((cm = GetStr()) != "q")
 					{
-						//status = mfrc522.PICC_HaltA();
-						status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &factorykey, &(mfrc522.uid));
-						if (status != MFRC522::STATUS_OK) {
+						// status = mfrc522.PICC_HaltA();
+						status = (MFRC522::StatusCode)mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &factory_key, &(mfrc522.uid));
+						if (status != MFRC522::STATUS_OK)
+						{
 							Serial.print(F("PCD_Authenticate() failed: "));
 							Serial.println(mfrc522.GetStatusCodeName(status));
 							break;
-						}else
+						}
+						else
 						{
-						Serial.print("Connected ");
-						Serial.println(mfrc522.GetStatusCodeName(status));
+							Serial.print("Connected ");
+							Serial.println(mfrc522.GetStatusCodeName(status));
 						}
 					}
 				}
-
-
 			}
-
-
-			//Serial.println(mfrc522.PICC_IsNewCardPresent());
-			//mfrc522.PICC_ReadCardSerial();
-			//Serial.println(mfrc522.uid);
-			//mfrc522.PICC_DumpDetailsToSerial(&mfrc522.uid);
-			
-			/*
-
-			// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-			if ( ! mfrc522.PICC_IsNewCardPresent()) {
-				continue;
-			}
-
-			// Select one of the cards
-			if ( ! mfrc522.PICC_ReadCardSerial()) {
-				continue;
-			}
-			mfrc522.PICC_DumpDetailsToSerial(&mfrc522.uid);
-
-			//PICC_Select
-			status = mfrc522.PICC_HaltA();
-
-			Serial.println(mfrc522.GetStatusCodeName(status));
-			mfrc522.PCD_StopCrypto1();
-			*/
-			
-			
-
 		}
 		Serial.println("quiting");
 		return MENU;
@@ -173,131 +139,103 @@ class Cardnear : public State
 public:
 	virtual State *main()
 	{
-		Serial.println("Card near");
+		Serial.println("Continous card check test");
 		Serial.println("commands:");
 		Serial.println("q for quit");
+		Serial.println("A to send it to sleep for quit");
 		String cm;
 		while ((cm = GetStr()) != "q")
 		{
-			if (cm != "")
+			if (cardservice.Check_card())
 			{
-				if(cm == "s")
-				{
-					Keep_card(2);
-				}
-
+				Serial.print("Y");
+				tone(22, 440, 100);
 			}
+			else if (cardservice.Get_new_card())
+			{
+				Serial.println("New card is present.");
+			}
+			else
+			{
+				Serial.println("No card near");
+				Serial.println(aut_counter);
+				noTone(22);
+			}
+
+			delay(100);
 		}
 		Serial.println("quiting");
 		return MENU;
 	}
-
-	bool is_equal(byte* a,byte*b,byte size)
-	{
-		for(byte i=0;i<size;i++)
-		{
-			if(a[i] != b[i])
-				return false;
-		}
-		return true;
-	}
-
-
-	bool Keep_card(int timeout = 1)//timeout in sec
-	{
-		int timeout_ms = timeout*1000;
-		unsigned long endtime = millis() + timeout*1000;
-
-		byte selected_uid[10] = {0};
-		bool is_empty = true;
-		bool is_active = false;
-
-		byte blockAddr = 11;
-  		byte trailerBlock = 11;
-
-
-		while(endtime > millis()) //until timeout is not reached, timeout restart when card is detected
-		{
-			if(!is_active)
-			{
-
-			if(mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
-			{
-				//new card is in the reader and ready state
-				//get the id
-				if(is_empty)
-				{
-					is_empty = false;
-					memcpy(selected_uid,mfrc522.uid.uidByte,mfrc522.uid.size);
-				}else
-				{
-					if(is_equal(mfrc522.uid.uidByte,selected_uid,mfrc522.uid.size))
-						{
-							is_active = true;
-						}else
-						{
-							Serial.println("not equal card id");
-						}
-				}
-			}
-			}else
-			{
-				status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &factorykey, &(mfrc522.uid));
-				if (status != MFRC522::STATUS_OK) {
-					Serial.print(F("PCD_Authenticate() failed: "));
-					Serial.println(mfrc522.GetStatusCodeName(status));
-					is_active = false;
-					continue;
-				}else
-				{
-					Serial.print("Connected ");
-					Serial.println(mfrc522.GetStatusCodeName(status));
-					endtime = millis() + timeout*1000;
-				}
-			}
-
-
-		}
-			Serial.println("Card keep timeout");
-			return false;
-
-
-
-	}
-
-
-
-
-
-
 };
 
+class Read_write : public State
+{
+public:
+	Profile testprofile;
 
+	Read_write()
+	{
+
+	};
+
+
+	virtual State *main()
+	{
+		Serial.println("Read_write test");
+		Serial.println("commands:");
+		Serial.println("q for quit");
+		Serial.println("");
+   cardservice.check_module_connection();
+		String cm;
+		while (true)
+		{
+			while (cardservice.Check_card());
+			while (!cardservice.Get_new_card())
+				delay(300);
+			tone(22, 440, 100);
+			while (cardservice.Check_card())
+			{
+				cardservice.Read_profile_from_card(&testprofile);
+				testprofile.print();
+				testprofile.name = "card C \0";
+				testprofile.balance = 100;
+				testprofile.transaction_count = 1234;
+				testprofile.uid = 3;
+				cardservice.Write_profile_to_card(&testprofile,true);
+				delay(100);
+				break;
+			}
+			break;
+		}
+		Serial.println("quiting");
+		return MENU;
+	}
+};
 
 Menu menu_class;
 Listdetect listdetect_class;
 Cardnear cardnear_class;
+Read_write read_write_class;
 
 //*****************************************************************************************//
 void setup()
 {
-
 	MENU = &menu_class;
 	LISTDETECT = &listdetect_class;
 	CARDNEAR = &cardnear_class;
+	READ_WRITE = &read_write_class;
 	state = MENU;
 
 	Serial.begin(9600); // Initialize serial communications with the PC
-	SPI.begin();		// Init SPI bus
-	mfrc522.PCD_Init(); // Init MFRC522 card
-	Serial.setTimeout(100);
-	Serial.println(F("Read PICC:")); // shows in serial that it is ready to read
+	cardservice.init();
 
-	Serial.print("factory key: ");
-	printkey(factorykey);
+	Serial.setTimeout(100);
+	Serial.print("factory_key: ");
+	printkey(factory_key);
 	Serial.println();
-	Serial.print("mykey: ");
-	printkey(mykey);
+	Serial.print("Prod_key: ");
+	printkey(Prod_key);
 	Serial.println();
 }
 
