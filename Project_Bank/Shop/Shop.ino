1b174c8fa9a40749afb99edd498398ceb6107ff1
SchmidtLaci (Warden) <schmidtlacus@gmail.com>
2023-06-20 15:27:36 +0200

    Started Shop, fixed termelo

--- Project_Bank/Shop/Shop.ino
+++ Project_Bank/Shop/Shop.ino
@@ -1,18 +1,17 @@
 /*
-2. eszköz termelő állomás
-áramot kap az eszköz, zöld ledet világít
-behelyezik a kártyát, kijelzőn megjelenik a termelés indítása felirat
-megnyomják a start gombot "termelés indul(esetleg egy betöltő csík mehetne a kijelzőn) 30 percig "tölt"
-közbe sárga led világít (villog?),
-ha lejárt a kártyára +500(hasra ütöttem az összeget) felíródik
-a kártyára írás közben piros led és ne vegye ki a kártyát felirat,
-majd vissza kezdő állapotba, zöld led, startra vár
+1. eszköz bolt,
+2 kijelző (egy eladó egy ügyfélnek) ugyanazt írja ki, | this need modification on the i2c board
+beolvasáskor kiírja a kijelzőkre a nevet és a rajta lévő összeget,
+a bolt oldalán 6 gomb, +100,+200,+500 és -100,-200,-500
 */
 
+#define DUAL_LCD_SCREEN
+
+#include "include/input.h"
 #include "include/display.h"
 #include "include/CardService.h"
 
-CardService cardservice; //  9,10 pin
+CardService cardservice; 
 
 Profile temp_profile;
 
@@ -21,21 +20,30 @@ Profile temp_profile;
 // DO NOT GIVE BIGGER NAME THAN THIS, IT WILL CAUSE THE SD CARD TO GO KERNEL PANIC
 #define LOGFILE_NAME "TERM0.log"
 
-#define button_pin A3
+#define button_pin_p1 A3
+#define button_pin_p2 A4
+#define button_pin_p5 A5
+#define button_pin_m1 A6
+#define button_pin_m2 A7
+#define button_pin_m3 A8
+#define button_pin_enter A9
 
 #define zold_pin A0
 #define sarga_pin A1
 #define piros_pin A2
 
-#define termeles_jutalom 500
-
-bool isbuttonDown = false;
-
-uint32_t termeles_time = 5;
-
 LED led_zold(zold_pin);
 LED led_sarga(sarga_pin);
 LED led_piros(piros_pin);
+
+Button btn_p1(button_pin_p1);
+Button btn_p2(button_pin_p2);
+Button btn_p5(button_pin_p5);
+Button btn_m1(button_pin_m1);
+Button btn_m2(button_pin_m2);
+Button btn_m3(button_pin_m3);
+Button btn_enter(button_pin_enter);
+
 
 void Error_loop()
 {
@@ -65,8 +73,6 @@ void setup()
 		Error_loop();
 	}
 
-	pinMode(button_pin, INPUT_PULLUP); // active low button
-
 	led_zold.turn(on); // if all good green led turned on
 	logn("Connected!");
 	
@@ -77,11 +83,7 @@ void setup()
 
 void loop()
 {
-	/**
-	 * @brief After start or succesfull termeles the device swiched to idle state
-	 * Waiting for card to be inserted.
-	 *
-	 */
+	
 clear_card:
 	led_sarga.turn(off);
 	led_piros.turn(off);
@@ -98,6 +100,8 @@ clear_card:
 	}
 	cardservice.Read_profile_from_card(&temp_profile);
 	temp_profile.print();
+
+
 	// megvárjuk a gombnyomást
 	logn("Waiting for Button press.");
 	lcd_write(temp_profile.name, "Termeles inditas");
@@ -130,37 +134,10 @@ clear_card:
 					// goto clear_card;
 		}
 	}
+
 termeles:
-	logn("termeles...");
- 
-	led_sarga.turn(on);
-	uint32_t end_time = millis() + 1000 * termeles_time;
-	lcd_write("Termeles:", "");
-	while (millis() < end_time)
-	{
-		int segment = int(16 - 16 * (float(end_time - millis()) / float(1000 * termeles_time)));
-		if (cardservice.Check_card())
-		{
-			// ez jó de nem kell csinálni semmit
-		}
-		else
-		{
-			lcd_write("Kartya elveszett", "Termelesi hiba.");
-			delay(2000);
-			return; // same as goto clear_card
-					// goto clear_card;
-		}
 
-		lcd_char(' ', 1, segment);
-		delay(500);
-		lcd_char('#', 1, segment);
-		led_sarga.togle();
-		delay(500);
-		// tune(100)
-	}
-	led_sarga.turn(off);
-
-	// termelés sikeres pénz kell a kártyára írni
+	// pénz kell a kártyára írni
 	led_piros.turn(on);
 	logn("Transaction.");
  
@@ -171,13 +148,13 @@ termeles:
 	if (!cardservice.Write_profile_to_card(&temp_profile))
 	{
 		// profile save failure, something went wrong
-		logn("Transaction fail.");
 		lcd_write("Transaction fail", "Contact IT");
 		delay(5000);
 		lcd_write("Device will", "Restart");
 		delay(5000);
 		return;
 	}
+ 	delay(1000);
  
 	if (!cardservice.Check_card())
 	{
@@ -186,10 +163,13 @@ termeles:
 
 	temp_profile.print();
 
-	static char str[80]; //more than enough
+	// 16 + 1 +
+	static char str[80];
 	//  log to card
 	//  name,uid,transaction_cunt,previous,added,new_balance
-	//longest string is
+
+	// 4294967296
+
 	//"aaaaaaaaaaaaaaaa,4294967296,4294967296,-2147483648,-2147483648,-2147483648R"
 
 	sprintf(str,"%s,%lu,%lu,%ld,%d,%ld\n",temp_profile.name,temp_profile.uid,temp_profile.transaction_count,previous_balance,termeles_jutalom,temp_profile.balance); // csv format
--- Project_Bank/include/display.h
+++ Project_Bank/include/display.h
@@ -7,22 +7,36 @@
 
 #define BUZZER 8
 
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd0(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+#ifdef DUAL_LCD_SCREEN
+// secondary display if needed
+LiquidCrystal_I2C lcd1(0x26, 16, 2); // You need to short the A0 pins on your i2c_lcd panel to change the addres to 0x26
+#endif
 
 void lcd_init()
 {
-	lcd.init();
-	lcd.backlight();
-	lcd.setCursor(0, 0);
-	lcd.print("");
-	lcd.setCursor(0, 1);
-	lcd.print("");
+	lcd0.init();
+	lcd0.backlight();
+	lcd0.setCursor(0, 0);
+	lcd0.print("");
+	lcd0.setCursor(0, 1);
+	lcd0.print("");
 	logn("LCD initilaized");
+	#ifdef DUAL_LCD_SCREEN
+	lcd1.init();
+	lcd1.backlight();
+	lcd1.setCursor(0, 0);
+	lcd1.print("");
+	lcd1.setCursor(0, 1);
+	lcd1.print("");
+	logn("Secoundary LCD initilaized");
+	#endif
 }
 
-void lcd_write(const char *line0_, const char *line1_)
+void lcd_write(const char *line0_, const char *line1_ , LiquidCrystal_I2C &lcd = lcd0)
 {
 	lcd.clear();
 	lcd.setCursor(0, 0);
@@ -30,7 +44,7 @@ void lcd_write(const char *line0_, const char *line1_)
 	lcd.setCursor(0, 1);
 	lcd.print(line1_);
 }
-void lcd_char(char c, short row, short col)
+void lcd_char(char c, short row, short col, LiquidCrystal_I2C &lcd = lcd0)
 {
 	lcd.setCursor(col, row);
 	lcd.print(c);
